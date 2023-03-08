0316995e3264a1edc1eb9f4cb0e7ee3e297abc63
valeriano alexander <valerianoalexander@gmail.com>
2023-03-08 11:55:42 +0100

    12 Playwright Configuration File

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  timeout: 30 * 1000,
+  retries: 0,
+  use: {
+    headless: true,
+    viewport: { width: 1024, height: 790 },
+    actionTimeout: 15 * 1000,
+    ignoreHTTPSErrors: true,
+    video: "off",
+    screenshot: "off",
+  },
+  projects: [
+    {
+      name: "Chrome",
+      use: { browserName: "chromium" },
+    },
+    {
+      name: "Firefox",
+      use: { browserName: "firefox" },
+    },
+    {
+      name: "Safari",
+      use: { browserName: "webkit" },
+    },
+  ],
+};
+
+export default config;
